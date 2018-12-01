0aa69324c6ec408fc1c7a54be915d3b49667fd31
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-12-01 02:30:14 +0100

    Add slick slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
+//navigation
 $(function() {
    $('#menu').slicknav({
       label: ''
+   });
+});
+
+// slider
+$(document).ready(function() {
+   $('.clients').slick({
+      autoplay: true,
+      speed: 100
    });
 });
