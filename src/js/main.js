8f6634ed447f19f300e43a3b04358c5dff4d7c28
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-12-01 04:32:44 +0100

    Add smoothscroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,27 @@
-//navigation
-$(function() {
-   $('#menu').slicknav({
-      label: ''
-   });
-});
-
-// slider
 $(document).ready(function() {
-   $('.clients').slick({
-      autoplay: true,
-      speed: 100
+   //navigation
+   $(function() {
+      $('#menu').slicknav({
+         label: ''
+      });
+   });
+
+   // slider
+   $(document).ready(function() {
+      $('.clients').slick({
+         autoplay: true,
+         speed: 100
+      });
+   });
+
+   // Smooth scroll
+   $('#menu a').smoothScroll({
+      afterScroll: function() {
+         $(this)
+            .closest('ul')
+            .find('a')
+            .removeClass('active');
+         $(this).addClass('active');
+      }
    });
 });
